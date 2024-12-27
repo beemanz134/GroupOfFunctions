78f64be9e9d33da94de068959a30f52065455df7
eggsNBakey <beeman134@gmail.com>
2024-12-26 23:04:35 -0600

    menu update 1
    added looping for inner and outer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod tempConverter;
+
 use std::io;
 
 fn main() {
@@ -11,7 +13,7 @@ fn main() {
         println!("press 6 for a web crawler ");
         println!("press 7 for file encryption/decryption ");
         println!("press 8 for a http server ");
-        println!("press 9 to exit");
+        println!("press 0 to exit");
 
         io::stdin()
             .read_line(&mut input)
@@ -20,12 +22,12 @@ fn main() {
             Ok(num) => num,
             Err(_) => {
                 println!("Please enter a number");
-                return;
+                continue; // Go back to the start of the loop
             }
         };
 
         match num {
-            1 => println!("1"),
+            1 => temperature_converter(), // No need to check for exit
             2 => println!("2"),
             3 => println!("3"),
             4 => println!("4"),
@@ -33,10 +35,37 @@ fn main() {
             6 => println!("6"),
             7 => println!("7"),
             8 => println!("8"),
-            9 => {
-                println!("breaking loop");
-                break;
+            0 => {
+                println!("Exiting the program");
+                break; // Breaks out of the outer loop
             }
+            _ => println!("unrecognized value"),
+        }
+    }
+}
+
+fn temperature_converter() {
+    loop {
+        let mut input = String::new();
+        println!("press 1 for fahrenheit -> celsius");
+        println!("press 2 for celsius -> fahrenheit");
+        println!("press 0 to exit the program");
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+
+        let num: u8 = match input.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a number");
+                continue; // Go back to the start of the inner loop
+            }
+        };
+
+        match num {
+            1 => tempConverter::convertFtC(),
+            2 => tempConverter::converCtF(),
+            0 => break, // Just break out of the inner loop
             _ => println!("unrecognized value"),
         }
     }
---
+++ src/tempConverter.rs
@@ -0,0 +1,7 @@
+pub fn convertFtC(){
+
+}
+
+pub fn converCtF(){
+
+}
