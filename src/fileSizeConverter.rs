9db8e57da3dde258d92b31d7c5357dfa8610c955
eggsNBakey <beeman134@gmail.com>
2025-01-10 13:52:54 -0600

    menu 4.2
    implemented decompression

--- src/fileSizeConverter.rs
+++ src/fileSizeConverter.rs
@@ -1,7 +1,7 @@
 use std::fs::File;
 use std::io::{Read, Write};
 use std::path::Path;
-use zip::{ZipWriter, write::FileOptions, CompressionMethod};
+use zip::{ZipWriter, write::FileOptions, CompressionMethod, ZipArchive};
 use std::io;
 use zip::write::ExtendedFileOptions;
 
@@ -33,7 +33,28 @@ pub fn compressFile() -> Result<(), Box<dyn std::error::Error>>{
 }
 
 pub fn deCompressFile() -> Result<(), Box<dyn std::error::Error>>{
-    Ok(())
+    loop{
+        let mut inp = String::new();
+        let mut oup = String::new();
+
+        println!("input 0 to exit");
+        println!("input file path to decompress");
+        io::stdin().read_line(&mut inp).expect("Failed to read line");
+        if inp.trim() == "0" || oup.trim() == "0" {
+            return Ok(());
+        }
+        println!("enter output file path");
+        io::stdin().read_line(&mut oup).expect("Failed to read line");
+        if inp.trim() == "0" || oup.trim() == "0" {
+            return Ok(());
+        }
+        if let Err(e) = decompress_file(inp.trim(), oup.trim()) {
+            eprintln!("An error occurred while decompressing from '{}' to '{}': {}", inp.trim(), oup.trim(), e);
+        } else {
+            println!("Successfully decompressed file from '{}' to '{}'", inp.trim(), oup.trim());
+        }
+
+    }
 }
 
 fn compress_to_zip(source_file: &str, zip_file: &str) -> Result<(), Box<dyn std::error::Error>> {
@@ -51,5 +72,20 @@ fn compress_to_zip(source_file: &str, zip_file: &str) -> Result<(), Box<dyn std:
     zipped.finish()?;
 
     println!("Compressing file: {} to {}", source_file, zip_file);
+    Ok(())
+}
+
+fn decompress_file(zip_file: &str, result_file: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let  source = File::open(zip_file)?;
+    let mut archive = ZipArchive::new(source)?;
+
+    for i in 0..archive.len(){
+        let mut zip_file = archive.by_index(i)?;
+        let output_path = format!("{}/{}", result_file, zip_file.name());
+
+        let mut output_file = File::create(&output_path)?;
+        io::copy(&mut zip_file, &mut output_file)?;
+    }
+
     Ok(())
 }
